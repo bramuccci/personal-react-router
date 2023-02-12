f34b0a6b640c7a6a8318f137cc61e6206237d501
Candela Bramucci <candelabramucci05@gmail.com>
2023-02-12 15:59:41 -0300

    removing unnecesary variable

--- router/Route.jsx
+++ router/Route.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import { PathContext } from './Router'
 
 export function Route({ path, element }) {
-    const { currentPath, setCurrentPath } = useContext(PathContext)
+    const { currentPath } = useContext(PathContext)
 
     if (currentPath == path) return element
 }
